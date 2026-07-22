e4a034ce524776254aa46e3372de26e063f9a8c4
Suemon-Kwok <149294826+Suemon-Kwok@users.noreply.github.com>
2026-07-23 10:04:02 +1200

    Initial files added

---
+++ AppointmentBooking/AppointmentBooking/AppointmentBookingService.cs
@@ -0,0 +1,13 @@
+﻿namespace ENSE707_AppointmentBooking
+{
+    public class AppointmentBookingService
+    {
+        public bool BookAppointment(AppointmentRequest request)
+        {
+            if (request.Doctor.AvailableSlots <= 0)
+                return false;
+            request.Doctor.AvailableSlots--;
+            return true;
+        }
+    }
+}
